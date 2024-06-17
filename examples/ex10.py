d1367f8c143c0170cf48e6a885cac5da954bc062
Vinod Kumar Kayartaya <vinod@vinod.co>
2024-06-17 16:30:46 +0530

    end of day 1

--- examples/ex10.py
+++ examples/ex10.py
@@ -1,19 +1,20 @@
 import time
 import json
+import csv
 
 
 def main():
     filename = input('Enter CSV filename: ')
-
     with open(filename) as file:
-        keys = file.readline().strip().split(',')
-
-        data = []
-        for each_line in file:
-            vals = each_line.strip().split(',')
-            data.append(dict(zip(keys, vals)))
-
-        print(data)
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+        
+    # with open(filename) as file:
+    #     keys = file.readline().strip().split(',')
+    #     data = []
+    #     for each_line in file:
+    #         vals = each_line.strip().split(',')
+    #         data.append(dict(zip(keys, vals)))
 
     out_filename = f'{filename[:-4]}_{time.time()}.json'
     with open(out_filename, 'w') as file:
